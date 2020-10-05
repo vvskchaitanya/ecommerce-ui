e13a05be409246d91da23e6dafdbed646cfac2c4
vvskchaitanya <vvskchaitanya@gmail.com>
2020-10-05 17:03:52 +0000

    changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app'
 
 @Component({
   selector: 'app-header',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(firestore: AngularFirestore,public auth: AngularFireAuth) {
+
+
+  }
+
+  login() {
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
 
   ngOnInit(): void {
   }
